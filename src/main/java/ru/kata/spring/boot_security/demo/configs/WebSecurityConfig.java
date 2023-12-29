188b30e08c22e98ef13cf6e4b94a0c88fcecb73f
AlexeyASokolov <vdobriyputyout@gmail.com>
2023-12-29 14:15:01 +0300

    .

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import ru.rishaleva.springBootSecurity.service.MyUserDetailsService;
 
 
 @Configuration
--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -1,9 +1,9 @@
-package ru.rishaleva.springBootSecurity.service;
+package ru.kata.spring.boot_security.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.rishaleva.springBootSecurity.Dao.RoleDao;
-import ru.rishaleva.springBootSecurity.model.Role;
+import ru.kata.spring.boot_security.demo.dao.RoleDao;
+import ru.kata.spring.boot_security.demo.model.Role;
 
 import java.util.List;
 
